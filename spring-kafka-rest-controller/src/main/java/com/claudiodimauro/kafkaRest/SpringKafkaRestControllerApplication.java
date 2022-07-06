6e6a2fd8a3f1cec67bc9d20e67246b3c1e0e521b
Claudio S.Di Mauro <claudio.dimauro@nttdata.com>
2022-07-06 16:13:45 +0200

    Added project spring-kafka-rest-controller

---
+++ spring-kafka-rest-controller/src/main/java/com/claudiodimauro/kafkaRest/SpringKafkaRestControllerApplication.java
@@ -0,0 +1,13 @@
+package com.claudiodimauro.kafkaRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaRestControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaRestControllerApplication.class, args);
+	}
+
+}
---
+++ spring-kafka-rest-controller/src/test/java/com/claudiodimauro/kafkaRest/SpringKafkaRestControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claudiodimauro.kafkaRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaRestControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
